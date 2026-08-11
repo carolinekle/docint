9359f6c6e9e38d365ce58f6b75e418013f7954e2
carolinekle <caroline.klewinowski@gmail.com>
2026-08-11 18:40:14 -0400

    continue chunking

--- ingest.py
+++ ingest.py
@@ -1,9 +1,10 @@
 import pymupdf4llm
 import pymupdf
 import pathlib
+from pathlib import Path
 
 def ingest_text(file):
-    md_text = pymupdf4llm.to_markdown(
+    data = pymupdf4llm.to_markdown(
         file, 
         write_images=True, 
         image_path="output_images", 
@@ -11,12 +12,18 @@ def ingest_text(file):
         use_layout=False, 
         show_progress=True
         )
-    
-    for page in md_text:
 
+
+    for page in data:
+        chunk = dict(
+            text = data["text"], 
+            page = data["page"], 
+            file = Path(data["file"]).name
+            )
+        return chunk
 """ 
-    pathlib.Path("output.md").write_bytes(md_text.encode())
- """
+pathlib.Path("output.md").write_bytes(md_text.encode())
+"""
 
 
 def ingest_images(file):
